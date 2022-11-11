7d60f4edfe38ef1981047ef08f3989860a0d45e4
mac21dee <Deepakkr21092000@gmail.com>
2022-11-11 22:04:19 +0530

    Assignment5 re

---
+++ Assignment5/classWork.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Ques1{
+    public static void main(String[] args)
+    {
+        // Take the size of vector as input from user 
+        Scanner input = new Scanner(System.in);
+        System.out.println("ENTER THE SIZE OF VECTOR ");
+        int size_of_vector = input.nextInt();
+        Vector stringsVector = new Vector(size_of_vector+1);
+        // Take the objects to be stored in the vector from the user 
+        System.out.println("ADD OBJECTS TO THE VECTOR ");
+        for(int i =0;i<=size_of_vector;i++)
+        {
+            String obj = input.nextLine();
+            stringsVector.addElement(obj);
+        }
+        // input.close();
+        // Display the values of vector
+        System.out.println("PRINT THE VALUES OF VECTOR ");
+        for(int i =0;i<=size_of_vector;i++)
+        {
+           System.out.println(stringsVector.elementAt(i));
+        }
+        input.close();
+    }
+}
